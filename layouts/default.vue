93670e903df553a0269abb13cccbd0447178c4f6
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-10-15 19:10:40 +0200

    feat(header): create header

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,24 @@ export default {}
 
 <template>
   <div>
-    <header class="absolute top-0 right-0 left-0 h-16 px-4 md:px-16"></header>
+    <header
+      class="
+        absolute
+        top-0
+        right-0
+        left-0
+        h-16
+        px-4
+        md:px-16
+        flex flex-row
+        justify-between
+        items-center
+        text-xl
+      "
+    >
+      <div>InsameeApp</div>
+      <div class="text-gray-500 font-segoe font-semibold text-lg">À propos</div>
+    </header>
     <Nuxt />
   </div>
 </template>
